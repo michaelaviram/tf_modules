b44782e193259eda2e89e459a3593cbd3c9b675d
michael.aviram <michael.aviram@gmail.com>
2025-05-29 09:59:35 +0300

    Added alb controller for eks basic cluster

---
+++ eks_basic/alb_controller/variables.tf
@@ -0,0 +1,18 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "Test-Cluster"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where the cluster is created"
+  type        = string
+  default = "vpc-06774b48185afd13e"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
